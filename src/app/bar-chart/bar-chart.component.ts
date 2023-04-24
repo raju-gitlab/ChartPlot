b2955e76f485e07be096f15c2ce27a8d38423ebd
raju-gitlab <rajubasak.gmp@gmail.com>
2023-04-24 23:27:53 +0530

    .

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -17,8 +17,8 @@ export class BarChartComponent implements OnInit {
   public barChartType : any = 'bar';
   public barChartLegend = true;
   public barChartData = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+    {data: [-65, 59, 80, 81, -56, 55, 40], label: 'Series A'},
+    {data: [28, -48, -40, 19, -86, -27, -90], label: 'Series B'}
   ];
   ngOnInit() {
   }
