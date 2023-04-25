e992391cd10a8311ded700d67ed49733d343ee06
raju-gitlab <rajubasak.gmp@gmail.com>
2023-04-25 23:16:53 +0530

    Line Chart

--- src/app/line-chart/line-chart.component.ts
+++ src/app/line-chart/line-chart.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-line-chart',
   templateUrl: './line-chart.component.html',
   styleUrls: ['./line-chart.component.css']
 })
-export class LineChartComponent {
+export class LineChartComponent implements OnInit {
+  canvas: any = ElementRef;
+  chartType: string = 'line';
+  chartData: any = [];
+  chartLabels: any = [];
+  chartColors: any = [];
+  public chartOptions: any;
 
+  ngOnInit() : void {
+    this.chartData = [{
+      data: [30, -114, 4.5, -22, 5], fill: {
+        target: 'origin',
+        above: 'rgba(255,193,206, 0.6)',
+        below: 'rgba(255,193,206, 0.6)'
+      },
+      borderColor: "rgba( 255,193,206, 0.7)",
+      tension : "0.5",
+      pointRadius: 5
+    }];
+    this.chartLabels = ['Jan', 'Feb', 'Mar', 'Apr', 'May'];
+  }
 }
